3a8f1fdde0b5da25209392c19ae9ff45646f4700
Fernand Tan <ferdyboy81@gmail.com>
2016-07-25 14:46:49 -0700

    first commit

---
+++ db/migrate/20160725213822_create_dogs.rb
@@ -0,0 +1,13 @@
+class CreateDogs < ActiveRecord::Migration
+  def change
+    create_table :dogs do |t|
+      t.string :breed
+      t.string :age
+      t.string :sex
+      t.boolean :vaccinated
+      t.string :image
+
+      t.timestamps null: false
+    end
+  end
+end
