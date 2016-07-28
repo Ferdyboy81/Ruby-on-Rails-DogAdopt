6d50d896f024f6c163958185ce84c6cb86f6106f
Fernand Tan <ferdyboy81@gmail.com>
2016-07-28 12:13:20 -0700

    added admin registration

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-\# encoding: UTF-8
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160725215513) do
+ActiveRecord::Schema.define(version: 20160728184515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20160725215513) do
     t.datetime "updated_at",                          null: false
     t.string   "first_name"
     t.string   "last_name"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
